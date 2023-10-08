b066bb1c3e7d473953436d7f32c3927291ea2391
Charlotte <richter_charlotte@t-online.de>
2023-10-08 09:46:05 +0200

    In Datei / Aus Datei

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/*
+In Kommandozeile schreiben zum
+    a) chiffrieren
+        gcc main.c
+        ./a.out c Praktische_Physik.txt 
+        (chiffrierter Text wird in Datei Crypt.txt geschrieben)
+    b) dechiffrieren
+        gcc main.c
+        ./a.out d Crypt.txt
+        (dechiffrierter Text wird in Datei Clear.txt geschrieben)
+    
+    Schlüsselwort kann bliebig vergeben werden, ohne Umlaute, sollte natürlich zum ver- und entschlüsseln dasselbe sein
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +35,6 @@ char * code(char*text, char*key) {
     //Verschlüsseln
     for(int i = 0; i < strlen(text); i++) {
         int x = 0; //Werte egal
-        int y = 0;
 
         //key umwandeln, enthält keine Leerzeichen
         if(key[i] >= 'a' && key[i] <= 'z') {
@@ -39,7 +53,6 @@ char * code(char*text, char*key) {
             } else {
             crypt[i] = text[i] + x;
             }
-            //y = text[i] - 'a';
         }
         else if (text[i] >= 'A' && text[i] <= 'Z') {
             if (text[i] + x > 90) {
@@ -47,15 +60,12 @@ char * code(char*text, char*key) {
             } else {
             crypt[i] = text[i] + x;
             }
-            //y = text[i] - 'A';
         }
         else if (text[i] == 32) {                   //falls Leerzeichen dann lassen
             crypt[i] = '_';
         } else {
-            crypt[i] = text[i];                    //falls Sonderzeichen dann ok, 100 random nummer außerhalb alphabet festgelegt
+            crypt[i] = text[i];                    //falls Sonderzeichen dann übernehmen
         }                         
-
-    
     }
 
     return crypt;           
@@ -107,7 +117,7 @@ char * decode(char*text, char*key) {
         else if (text[i] == '_') {                   //falls Leerzeichen dann lassen
             neutext[i] = ' ';
         } else {
-            neutext[i] = text[i];                    //falls Sonderzeichen dann ok, 100 random nummer außerhalb alphabet festgelegt
+            neutext[i] = text[i];                    //falls Sonderzeichen dann übernehmen
         }   
     }
 
@@ -115,6 +125,7 @@ char * decode(char*text, char*key) {
 }
 
 //Funktion zur Ausgabe von strings (über printf("%s") würde bei erstem Leerzeichen abbrechen)
+/*
 void print(char* text) {
     for(int i = 0; i<strlen(text); i++) {
         printf("%c", text[i]);  
@@ -122,51 +133,100 @@ void print(char* text) {
     printf("\n");
     return;
 }
+*/
 
-int main() {
+//Funktion zur Ausgabe in Datei
+void printFile(char* text, FILE * filename) {
+    for(int i = 0; i<strlen(text); i++) {
+        fprintf(filename, "%c", text[i]);  
+    }
+    //fprintf(filename, "\n");
+    return;
+}
 
-//Urtext eingeben
-    int textlength = 100;               //maximale string Länge
+
+int main(int argc, char **argv) {
+
+        //eine Eingebae akzeptiert (argc = 2)
+        if(argc < 2 || argc > 3) { //argc = 0 immer Betriebssystem, arc = 1 ist erste Eingabe
+        printf("Usage: no valid imput \n");
+        exit(1);
+        }
+
+//Verschlüsseln
+if (strcmp(argv[1], "c") == 0) {
+    
+    //open urdatei die verschlüsselt werden soll
+    char* dateiname = argv[2];
+    FILE * urdatei = fopen(dateiname, "r");
+
+    //open cryptdatei in die verschlüsselter text geschrieben werden soll
+    FILE * cryptdatei = fopen("Crypt.txt", "w");
+
+    //speicher reservieren für herausgelesene Zeilen aus urdatei
+    int textlength = 200;               //maximale string Länge, hoffentlich ist keine Zeile länger als 200 Zeichen
     char * urtext = malloc(textlength); //Speicherplatz reservieren, urtext ist pointer auf array Beginn
 
-    printf("Zu verschlüsselnden Text eingeben: \n");
-    scanf("%100[^\n]s", urtext);    //string abbruch bei Zeilenumbruch, maximal 100 character
-
-    //printf("Eingegeben ist: %s\n", urtext); //Kontrolle
-
-//Schlüsselwort eingeben
-
+    //Schlüsselwort (key) vergeben
     char * key = malloc(textlength); //Speicherplatz reservieren, urtext ist pointer auf array Beginn
-
     printf("Schlüsselwort (ohne Leerzeichen) eingeben: \n");
     scanf("%100s", key);    //string abbruch bei Zeilenumbruch, maximal 100 character
 
-    //printf("Eingegeben ist: %s\n", key); //Kontrolle
+    while(!feof(urdatei)) {
+        fgets(urtext, textlength, urdatei);  //Zeile in urtext-array geschrieben
 
-//Verschlüsselung
+        char * crypt = malloc(strlen(urtext));   
+        crypt = code(urtext, key);
 
-    char * crypt = malloc(strlen(urtext));   
-    crypt = code(urtext, key);              //bricht stringinput bei Leerzeichen ab?
+        printFile(crypt, cryptdatei);
+        free(crypt);
+    }
 
-    //Verschlüsselten Text ausgeben -> evtl. in Datei später?
-    print(crypt);
-
-//Entschlüsseln / crypt - key = urtext
-
-    //Schlüsselwort neu eingeben //Speicher schon oben reserviert
-    printf("Schlüsselwort (ohne Leerzeichen) eingeben: \n");
-    scanf("%100s", key);    //string abbruch bei Zeilenumbruch, maximal 100 character
-
-    //Speicher für neue Ausgabe reservieren in Größe von Urtext
-    char * neutext = malloc(strlen(urtext));
-    neutext = decode(crypt, key);   
-
-    //entschlüsselten Text ausgeben -> evtl. in Datei später?
-    print(neutext);
-
-//dynamisch reservierten Speicher freigeben
-    free(crypt);
     free(urtext);
     free(key);
+    fclose(urdatei);
+    fclose(cryptdatei);
+}
+
+//Dechiffrieren
+else if (strcmp(argv[1], "d") == 0) {
+
+    //open datei die entschlüsselt werden soll
+    char* dateiname = argv[2];
+    FILE * cryptdatei = fopen(dateiname, "r");
+
+    //open enddatei in die entschlüsselter text geschrieben werden soll
+    FILE * neudatei = fopen("Clear.txt", "w");
+
+    //speicher reservieren für herausgelesene Zeilen aus urdatei
+    int textlength = 200;               //maximale string Länge, hoffentlich ist keine Zeile länger als 200 Zeichen
+    char * crypt = malloc(textlength); //Speicherplatz reservieren, urtext ist pointer auf array Beginn
+
+    //Schlüsselwort (key) eingeben
+    char * key = malloc(textlength); //Speicherplatz reservieren, urtext ist pointer auf array Beginn
+    printf("Schlüsselwort (ohne Leerzeichen) eingeben: \n");
+    scanf("%100s", key);    //string abbruch bei Zeilenumbruch, maximal 100 character
+
+    while(!feof(cryptdatei)) {
+        fgets(crypt, textlength, cryptdatei);  //Zeile in urtext-array geschrieben
+
+        char * neutext = malloc(strlen(crypt));   
+        neutext = decode(crypt, key);
+
+        printFile(neutext, neudatei);
+        free(neutext);
+    }
+    
+    free(crypt);
+    free(key);
+    fclose(neudatei);
+    fclose(cryptdatei);
+
+}
+
+else {
+    printf("Ungültige Eingabe, bitte c zum Chiffrieren und d zum Dechiffrieren eingeben, danach Dateiname\n");
+}
+    
 
 }
